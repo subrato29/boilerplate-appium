aa17cfd983e2f12adb84e613e7c39d79f2cf62dd
subrato29 <subratosarkar29@gmai.com>
2022-01-30 09:59:38 -0500

    testng running class modified

--- src/test/java/com/runner/testng/TestNgRunner.java
+++ src/test/java/com/runner/testng/TestNgRunner.java
@@ -8,12 +8,12 @@ import org.testng.TestNG;
 import com.beust.jcommander.internal.Lists;
 
 public class TestNgRunner {
-	public static void main(String[] args) {
-		DynamicTestNg.main(args);
-		TestNG testng = new TestNG();
-        List<String> suites = Lists.newArrayList();
+    public static void main(String[] args) {
+        DynamicTestNg.main(args);
+        TestNG testng = new TestNG();
+        List < String > suites = Lists.newArrayList();
         suites.add(System.getProperty("user.dir") + File.separator + DynamicTestNg.testNgXml);
         testng.setTestSuites(suites);
         testng.run();
-	}
+    }
 }
