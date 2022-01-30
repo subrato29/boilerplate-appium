f2ca56cbddbc27e20e5277db8f2dbfac17c3c6b4
subrato29 <subratosarkar29@gmai.com>
2022-01-30 12:49:28 -0500

    updated pageobject

--- src/test/java/pageobjects/Login.java
+++ src/test/java/pageobjects/Login.java
@@ -3,10 +3,10 @@ package pageobjects;
 import com.mobile.utilities.Util;
 
 public class Login {
-		public static String email = Util.locator("//android.widget.EditText[1]");
-		public static String password = Util.locator("//android.widget.EditText[2]");
-		public static String btnSignIn = Util.locator("//android.widget.Button[1]");
-		public static String txtAuthenticate = Util.locator("//android.widget.TextView[@text='Authenticate']");
-		public static String btnMaybeLater = Util.locator("//android.widget.Button[@text='MAYBE LATER']");
-		
+    public static String email = Util.locator("//android.widget.EditText[1]");
+    public static String password = Util.locator("//android.widget.EditText[2]");
+    public static String btnSignIn = Util.locator("//android.widget.Button[1]");
+    public static String txtAuthenticate = Util.locator("//android.widget.TextView[@text='Authenticate']");
+    public static String btnMaybeLater = Util.locator("//android.widget.Button[@text='MAYBE LATER']");
+
 }
